bbf308ae47bef85fe01474b5f9d701a888fef89d
Amir <baruch.amir@gmail.com>
2017-02-08 13:47:36 +0100

    basic default channels fetch in place

--- src/App.js
+++ src/App.js
@@ -1,20 +1,108 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      results: null,
+    };
+    this.fetchSearchStreams = this.fetchSearchStreams.bind(this);
+    this.setSearchResults = this.setSearchResults.bind(this);
+  }
+
+  fetchSearchStreams(){
+    console.log('fetchSearchStreams');
+    fetch("https://api.twitch.tv/kraken/search/channels?query=*",{
+      headers: {
+          "Client-ID": "bm7ifyx69m7j6yo8t39b43gb70etra"
+        },
+    })
+      .then(response => response.json())
+      .then(results => this.setSearchResults(results))
+      .catch(function(ex) {
+        console.log('parsing failed', ex)
+        })
+  }
+
+  setSearchResults(results){
+    this.setState({
+      results,
+    });
+    console.log('setting search results', results);
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    this.fetchSearchStreams();
+  }
   render() {
+    const {
+      results,
+    } = this.state;
+    console.log('app.render.results', results);
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Twitch Viewer</h2>
+        <Search />
+        <Table
+          results={results} />
       </div>
     );
+  }
+}
+
+// Search Component
+const Search = () => (
+  <form>
+    <input
+      type="text"/>
+    <button
+      type="submit">
+      search button
+    </button>
+  </form>
+);
+
+// Table Component
+class Table extends Component{
+  render(){
+    const {
+      results
+    } = this.props;
+    console.log('table.results:', results);
+    return(
+      <div>
+        <div className='Table-Header'>
+          <span>IMG</span>
+          <span>CHANNEL DISPLAY NAME</span>
+          <span>ONLINE</span>
+          <span>ONLINE</span>
+          <span>LINK</span>
+        </div>
+
+        { results ? (
+          results.channels.map(channel=>
+          <div key={channel._id} className='Table-row'>
+            <span>{channel._id}</span>
+            <span>{channel.display_name}</span>
+            <span>
+              <img src={channel.logo} alt="logo" height="42" width="42" />
+            </span>
+
+          </div>
+          )
+        )
+
+          : <span>No results</span>
+
+        }
+
+
+      </div>
+    )
   }
 }
 
